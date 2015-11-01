f7710d21bcbd5fd66c8303c944cf28b0c8cde21a
Josh Bubis <joshbubis@gmail.com>
2015-10-31 23:45:02 -0400

    for some reason the new messages won't append if added through chatbox

--- client/newMessage/newMessage.js
+++ client/newMessage/newMessage.js
@@ -1,8 +1,13 @@
 function createMessage(event) {
   Messages.insert({
-    message:event.target.message.value,
+    message: event.target.message.value,
     time: new Date()
   });
   event.target.message.value = '';
   return false;
+
+
+  Template.newMessage.events({
+    "submit form": createMessage
+  })
 }
